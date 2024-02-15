20416bcfa025c78d578cb1cf701b2b670340760e
DaviPedrosa <pedrosassss@hotmail.com>
2024-02-15 11:01:41 -0300

    Finalizando o exercicio 3

--- Parte_2/Exercício_3.java
+++ Parte_2/Exercício_3.java
@@ -1,11 +1,14 @@
 package Parte_2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Exercício_3 {
 
     public static void main (String [] args) throws Exception {
 
+        List<Produtos>carrinho = new ArrayList<>();
         int controlador = 0;
 
         /* Tabela de Preços */
@@ -17,6 +20,8 @@ public class Exercício_3 {
         System.out.printf("%nCodigo: 5     Refrigerante        Preco: R$1,50");
         System.out.printf("%n------------------------------------------------");
 
+        Scanner scanner = new Scanner (System.in);
+
         while (controlador == 0) {
 
             int cod;
@@ -24,7 +29,6 @@ public class Exercício_3 {
 
             /* Entrada */
             System.out.printf("%nDigite o codigo do produto: ");
-            Scanner scanner = new Scanner (System.in);
             cod = Integer.parseInt(scanner.nextLine());
 
             /* Controle de excessão */
@@ -49,15 +53,60 @@ public class Exercício_3 {
                     if (qtd < 0) {
                         throw new Exception();
                     }
+                    break;
                  } catch (Exception valInv) {
                     System.out.printf("%nValor inserido é invalido.");
                  }
             }
 
+            /* Método para adicionar produtos ao carrinho */
+            if(cod == 1){
+                carrinho.add(new Produtos(cod,qtd,4,qtd*4));
+            }
+            else if (cod == 2){
+                carrinho.add(new Produtos(cod,qtd,4.5,qtd*4.5));
+            }
+            else if (cod == 3){
+                carrinho.add(new Produtos(cod,qtd,5,qtd*5));
+            }
+            else if (cod == 4){
+                carrinho.add(new Produtos(cod,qtd,2,qtd*2));
+            }
+            else if (cod == 5){
+                carrinho.add(new Produtos(cod,qtd,1.5,qtd*1.5));
+            }
 
+            /* Verificando necessidade do cliente (Ira compra mais ou não) */
+            System.out.printf("%nDeseja adicionar mais algum produto?%nSe sim, digite 1, caso deseja encerrar a compra digite 0:%n");
+            int check;
+            check = Integer.parseInt(scanner.nextLine());
 
+            /* Criando o método que demonstrará a compra como um todo */
+            if (check == 0){
+
+                /* Variável que calculará o valor total da compra */
+                double valCompra = 0;
+
+                
+                System.out.printf("%n------------- Comanda -------------");
+
+                /* Percorrendo carrinho para repassar tudo aquilo que o cliente desejou comprar */
+                for (Produtos prod : carrinho){
+                    System.out.printf("%n%nCódigo: %d %nQuantidade: %d %nValor unitario: R$%.2f %nValor cumulativo: R$%.2f",prod.getCod(),prod.getQtd(),prod.getValor(),prod.getValorTotal());
+                    valCompra += prod.getValorTotal();
+                }
+
+                /* Saída final do programa */
+                System.out.printf("%n%nO valor total da compra: R$ %.2f",valCompra);
+                System.out.printf("%n%n------------- Comanda -------------");
+                System.out.printf("%n%nFavor seguir ao caixa para efetuar o pagamento!");
+
+                controlador += 1;
+            }
         }
+    
+        scanner.close();
 
     }
-
 }
+
--- Parte_2/Produtos.java
+++ Parte_2/Produtos.java
@@ -5,11 +5,29 @@ public class Produtos {
     private int cod;
     private int qtd;
     private double valor;
+    private double valorTotal;
 
-    public Produtos (int cod, int qtd, double valor) {
+    public Produtos (int cod, int qtd, double valor, double valorTotal) {
         this.cod = cod;
         this.qtd = qtd;
         this.valor = valor;
+        this.valorTotal = valorTotal;
+    }
+
+    public int getCod(){
+        return this.cod;
+    } 
+
+    public int getQtd(){
+        return this.qtd;
+    }
+
+    public double getValor(){
+        return this.valor;
+    }
+
+    public double getValorTotal(){
+        return this.valorTotal;
     }
     
 }
